7624fbf7706be3c5f6203421dcaa29a362028ba7
Nicolas-Asafe <nicolasasafe45@gmail.com>
2025-05-04 14:17:28 -0300

    Atualização da rota person

--- back/src/routers/users.routers.ts
+++ back/src/routers/users.routers.ts
@@ -6,13 +6,27 @@ import { middleware } from "../../utils/jwt.tools";
 const UserRouter = Router();
 
 UserRouter.get('/Users', Control_listUsers);
-UserRouter.get('/Person',middleware,async (req:any,res:any)=>{
-    const user:any = await UserModel.find({_id:req.id})
-    user?
-        res.status(200).json({ message: 'User listed succesfully', status: true, user:{Name:user.Name,Orgs:user.Orgs} }):
-        res.status(404).json({ message: 'error for get user', status: false});
-
-})
+UserRouter.get('/Person', middleware, async (req: any, res: any) => {
+    try {
+      const user = await UserModel.findById(req.id);
+  
+      if (user) {
+        res.status(200).json({
+          message: 'User listed successfully',
+          status: true,
+          user: {
+            Name: user.Name,
+            Orgs: user.Orgs
+          }
+        });
+      } else {
+        res.status(404).json({ message: 'User not found', status: false });
+      }
+    } catch (err:any) {
+      res.status(500).json({ message: 'Server error', status: false, error: err.message });
+    }
+  });
+  
 UserRouter.post('/NewUser', Control_createUser);
 UserRouter.post('/Login', Control_getAuth);
 UserRouter.delete('/DeleteUser', middleware, Control_deleteUser);
